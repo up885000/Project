08a4a27b07626b0c4288542c131b386ef3de37ec
up885000 <up885000@myport.ac.uk>
2022-03-03 12:33:40 +0000

    location Add

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var constraints = { video: { facingMode: "environment" }, audio: false },
     button = document.getElementById("mic-button"),
     textBox = document.getElementById("textBox"),
     recordedChunks = [],
+    location = null,
     chat = [];
     // chat.push([0,"me"]);
     // chat.push([0,"me"]);
